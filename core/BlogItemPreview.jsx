72d06175a9747356caa4e165dc986983797b7a50
nhathuy047 <146470435+nhathuy047@users.noreply.github.com>
2023-12-12 20:22:55 +0700

    Update BlogItemPreview.jsx

--- core/BlogItemPreview.jsx
+++ core/BlogItemPreview.jsx
@@ -8,7 +8,7 @@ export const BlogItemPreview = ({ title, img, categories, id }) => {
           <img
             src={img}
             alt={"blogimg"}
-            className="rounded-2xl"
+            className="rounded-2xl h-80"
             layout="fill"
             objectFit="cover"
           />
